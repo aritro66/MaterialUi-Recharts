d5e7fec671637a92a398ee9da26d8334400008f4
aritro66 <ghosharitro66@gmail.com>
2022-05-19 11:38:09 +0530

    update

--- src/components/drawer/UserInfo.jsx
+++ src/components/drawer/UserInfo.jsx
@@ -10,7 +10,7 @@ export default function UserInfo() {
     <Box sx={{ height: "60px", width: "100%" }}>
         <Divider />
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Avatar alt="Remy Sharp" sx={{ marginLeft: '20px', width: '38px', height: '38px' }}></Avatar>
+          <Avatar alt="" src="https://dwpdigital.blog.gov.uk/wp-content/uploads/sites/197/2016/07/P1090594-1.jpeg" sx={{ marginLeft: '20px', width: '38px', height: '38px' }}></Avatar>
           <Box sx={{ flexGrow: 1, marginLeft: '15px', padding: "10px 0" }}>
             <Typography component="div" variant="div"
               sx={{ fontSize: '15px', fontWeight: 'bold', color: theme.color1, fontFamily: 'sans-serif', }} >
